5e2791d5e6a3f0422750e7aa3e4516b69dae0ef5
Cameron Harvey <charvey0@gmail.com>
2021-03-28 15:11:36 -0700

    Added seeds for all three tables

--- seeds/index.js
+++ seeds/index.js
@@ -10,16 +10,17 @@ const postData = require('./post-seeds.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Comment.bulkCreate(commentData, {
-    individualHooks: true,
-    returning: true,
-  });
-
   await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
   await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
     individualHooks: true,
     returning: true,
   });
