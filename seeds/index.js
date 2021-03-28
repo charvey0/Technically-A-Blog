f35f708e6654ccbc123a918ec99ba7b8ba8fcb17
Cameron Harvey <charvey0@gmail.com>
2021-03-28 14:03:26 -0700

    Added basic structure

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/connection');
+const Comment = require('../models/Comment');
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+const commentData = require('./comment-seeds.json');
+const userData = require('./user-seeds.json');
+const postData = require('./post-seeds.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
