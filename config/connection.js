5e93a3a2d5918e4d6b2f32799c35d6ce5c5d0797
Cameron Harvey <charvey0@gmail.com>
2021-04-05 04:31:09 -0700

    fixed connection.js bug

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,15 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+const Sequelize = require('sequelize');
 
-{
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": 'localhost',
-    "dialect": "mysql",
-    "port": 3306,
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
-});
-
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
